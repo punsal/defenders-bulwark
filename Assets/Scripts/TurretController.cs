1dd37068ef0371c1142860ba150104170c4f016d
Mustafa Pamir Ünsal <pamir.uensal@sunday.gg>
2025-01-25 01:49:41 +0300

    Character controller is fixed

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -5,29 +5,11 @@ public class TurretController : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 100f;
     
-    private float _targetAngle = 0f;
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            _targetAngle = 0f;
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            _targetAngle = 90f;
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            _targetAngle = 180f;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            _targetAngle = 270f;
-        }
+        var rotDirection = Input.GetAxis(("Horizontal"));
         
-        float currentAngle = Mathf.LerpAngle(transform.eulerAngles.z, _targetAngle, rotationSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(0f, 0f, currentAngle);
+        transform.Rotate(new Vector3(0f, 0f, -1f * rotDirection * rotationSpeed * Time.deltaTime));
         
     }
 }
